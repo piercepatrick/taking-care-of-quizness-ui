d7c1388e6704fb189bdbe2b4d808a243eaef68b1
Mark Fulton <mark.fulton@asurion.com>
2021-02-10 15:35:08 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Card from "@material-ui/core/Card";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+
+      <Button color="primary">Hello World</Button>
+
+      <Card></Card>
+    </>
   );
 }
 
