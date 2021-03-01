3bfba5f05090f599fbf48e980725386ee7c86935
T Michael Smith <timothy.smith2@asurion.com>
2021-03-01 10:37:38 -0600

    useFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url, options) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+        setResponse(json);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { response, error, isLoading };
+};
+
+export default useFetch;
