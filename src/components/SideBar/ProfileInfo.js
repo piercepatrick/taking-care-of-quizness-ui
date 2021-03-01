0071639532d10fe06aabf746d66f37b8e65451fe
T Michael Smith <timothy.smith2@asurion.com>
2021-03-01 11:48:58 -0600

    padding bottom of profile info in drawer

--- src/components/SideBar/ProfileInfo.js
+++ src/components/SideBar/ProfileInfo.js
@@ -1,9 +1,17 @@
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography, makeStyles } from "@material-ui/core";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
+const useStyles = makeStyles({
+  root: {
+    paddingBottom: "1em",
+  },
+});
+
 function ProfileInfo({ userName }) {
+  const classes = useStyles();
+
   return (
-    <Container>
+    <Container className={classes.root}>
       <AccountCircleIcon color="secondary" fontSize="large" />
       <Typography variant="h5">{userName}</Typography>
     </Container>
