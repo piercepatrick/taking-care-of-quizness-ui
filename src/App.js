d44e569a06056c07427d4a05c9258c843f72b9ba
T Michael Smith <timothy.smith2@asurion.com>
2021-03-04 11:26:39 -0600

    added auth config and sign out functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { ThemeProvider } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import Amplify from "aws-amplify";
 
 import NewQuestion from "./components/NewQuestion/NewQuestion";
 import QuizView from "./components/QuizView/QuizView";
 import SideBar from "./components/SideBar/SideBar";
 import { theme } from "./theme";
+import { AuthConfig } from "./authentication/config";
+
+Amplify.configure(AuthConfig);
 
 function App() {
   return (
@@ -27,4 +32,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/components/SideBar/Navigation.js
+++ src/components/SideBar/Navigation.js
@@ -10,7 +10,9 @@ import AddIcon from "@material-ui/icons/Add";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
 import SettingsIcon from "@material-ui/icons/Settings";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { Link } from "react-router-dom";
+import { Auth } from "aws-amplify";
 
 const useStyles = makeStyles({
   navItem: {
@@ -25,55 +27,76 @@ const useStyles = makeStyles({
   navText: {
     textDecoration: "none",
   },
+  signOut: {
+    alignSelf: "flex-end",
+    backgroundColor: "#6200EE",
+    "& > *": {
+      color: "#FFFFFF",
+    },
+    "&:hover": {
+      backgroundColor: "#F2E7FE",
+    },
+    "&:hover > *": {
+      color: "#6200EE",
+    },
+  },
 });
 
 function Navigation() {
   const classes = useStyles();
 
+  const signOut = async () => {
+    try {
+      await Auth.signOut({ global: true });
+      window.location.reload();
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
   return (
     <div>
-      <div>
-        <Divider variant="fullWidth" />
-        <List>
-          <ListItem
-            component={Link}
-            to="/new"
-            button
-            className={classes.navItem}
-          >
-            <ListItemIcon>
-              <AddIcon />
-            </ListItemIcon>
-            <ListItemText primary="Add new question" />
-          </ListItem>
-          <ListItem
-            component={Link}
-            to="/quiz"
-            button
-            className={classes.navItem}
-          >
-            <ListItemIcon>
-              <QuestionAnswerIcon />
-            </ListItemIcon>
-            <ListItemText className={classes.navText} primary="My questions" />
-          </ListItem>
-        </List>
-        <Divider variant="fullWidth" />
-        <List>
-          <ListItem button className={classes.navItem}>
-            <ListItemIcon>
-              <SettingsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Account settings" />
-          </ListItem>
-          <ListItem button className={classes.navItem}>
-            <ListItemIcon>
-              <NotificationsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Notifications" />
-          </ListItem>
-        </List>
-      </div>
+      <Divider variant="fullWidth" />
+      <List>
+        <ListItem component={Link} to="/new" button className={classes.navItem}>
+          <ListItemIcon>
+            <AddIcon />
+          </ListItemIcon>
+          <ListItemText primary="Add new question" />
+        </ListItem>
+        <ListItem
+          component={Link}
+          to="/quiz"
+          button
+          className={classes.navItem}
+        >
+          <ListItemIcon>
+            <QuestionAnswerIcon />
+          </ListItemIcon>
+          <ListItemText className={classes.navText} primary="My questions" />
+        </ListItem>
+      </List>
+      <Divider variant="fullWidth" />
+      <List>
+        <ListItem button className={classes.navItem}>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Account settings" />
+        </ListItem>
+        <ListItem button className={classes.navItem}>
+          <ListItemIcon>
+            <NotificationsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Notifications" />
+        </ListItem>
+        <ListItem button className={classes.signOut} onClick={() => signOut()}>
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText primary="Sign Out" />
+        </ListItem>
+      </List>
     </div>
   );
 }
