36047c91ea96c7250246a480f7abe23f478703e5
T Michael Smith <timothy.smith2@asurion.com>
2021-03-05 14:36:37 -0600

    brute force solution for answer selection functionality

--- src/components/QuizView/QuizAnswers.js
+++ src/components/QuizView/QuizAnswers.js
@@ -1,23 +1,76 @@
+import { useState } from "react";
 import { Button, Grid, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
-  answer: { width: "100%", height: "100%" },
+  answer: {
+    width: "100%",
+    height: "100%",
+  },
+  correct: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#C8FFF4",
+  },
+  incorrect: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#F5DBDB",
+  },
 }));
 
 function QuizAnswers({ answers }) {
   const classes = useStyles();
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const renderAnswerButton = (answer) => {
+    if (selectedAnswer && selectedAnswer.value === answer.value) {
+      if (answer.isCorrect) {
+        return (
+          <Button
+            className={classes.correct}
+            variant="contained"
+            disableElevation
+            onClick={() => {
+              setSelectedAnswer(answer);
+            }}
+          >
+            {answer.value}
+          </Button>
+        );
+      }
+      return (
+        <Button
+          className={classes.incorrect}
+          variant="contained"
+          disableElevation
+          onClick={() => {
+            setSelectedAnswer(answer);
+          }}
+        >
+          {answer.value}
+        </Button>
+      );
+    } else {
+      return (
+        <Button
+          className={classes.answer}
+          variant="outlined"
+          color="secondary"
+          onClick={() => {
+            setSelectedAnswer(answer);
+          }}
+        >
+          {answer.value}
+        </Button>
+      );
+    }
+  };
 
   return (
     <Grid container spacing={2}>
-      {answers.map((value) => (
-        <Grid item xs={12} sm={6} key={value}>
-          <Button
-            className={classes.answer}
-            variant="outlined"
-            color="secondary"
-          >
-            {value}
-          </Button>
+      {answers.map((answer) => (
+        <Grid item xs={12} sm={6} key={answer.value}>
+          {renderAnswerButton(answer)}
         </Grid>
       ))}
     </Grid>
